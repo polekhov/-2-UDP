87ce2b5ec307eaa4d6ad609fd06b93db45991441
polekhov <lexa.polexow@yandex.ru>
2017-11-27 19:48:28 +0300

    Add files via upload

---
+++ Лабораторная работа 2/Работа 2 - сервер/Работа 2 - сервер/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work2Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 1255;
+
+            UdpClient socket = new UdpClient(port); //Создаём сокет
+            IPEndPoint senderEndPoint = null; //Здесь будет в результате находиться адрес отправителя
+
+            Console.WriteLine("Ожидаю сообщения");
+            byte[] buffer = socket.Receive(ref senderEndPoint); //Принимаем сообщение
+            DateTime clientTime = DateTime.FromBinary(BitConverter.ToInt64(buffer, 0)); //Переводим массив байт в DateTime
+            Console.WriteLine("Время на клиенте: " + clientTime.ToString("dd.MM.yyyy HH:mm:ss"));
+            DateTime time = DateTime.Now; //Получаем текущее время на сервере
+            Console.WriteLine("Время на сервере: " + time.ToString("dd.MM.yyyy HH:mm:ss"));
+            TimeSpan dtime = time - clientTime;
+            long dtimeMillis = dtime.Ticks / 10000;
+            Console.WriteLine("Разница во времени " + dtimeMillis + " ms");
+            buffer = BitConverter.GetBytes(dtimeMillis);
+            socket.Send(buffer, buffer.Length, senderEndPoint);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лабораторная работа 2/Работа 2 клиент/Работа 2 клиент/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work2Client
+{
+    class Program {
+        static void Main(string[] args) {
+            //Это адрес сервера и порт. Аналогичные должны быть записаны на сервере
+            IPAddress serverAddress = IPAddress.Parse("127.0.0.1");
+            int port = 1255;
+
+            //Формируем сообщение
+            DateTime time = DateTime.Now;
+            Console.WriteLine("Моё время " + time.ToString("dd.MM.yyyy   HH:mm:ss"));
+
+            /*
+             * Отправляем сообщение на сервер
+             */
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);   //Создаём UDP сокет
+            IPEndPoint serverEndPoint = new IPEndPoint(serverAddress, port); //"Пункт назначения" пакета данных
+            
+
+            byte[] bytes = BitConverter.GetBytes(time.ToBinary());//Переводим текущее время на компьютере в массив байт, который будем отправлять
+            socket.SendTo(bytes, serverEndPoint); //Отправляем получившийся массив байт
+
+            //Создаём буфер байт и принимаем в него разницу во времени
+            byte[] buffer = new byte[sizeof(long)];
+            socket.Receive(buffer);
+			socket.Close();
+
+            //Переводим байты во время
+            Console.WriteLine("Разница во времени с сервером: " + BitConverter.ToInt32(buffer, 0) + "ms");
+
+            Console.ReadKey();
+        }
+    }
+}
